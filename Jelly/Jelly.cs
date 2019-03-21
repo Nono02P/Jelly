e4b2d76fa61233b1de25fe7780d2127e81073a5d
Nono02P <nono02p@hotmail.fr>
2019-03-21 22:40:21 +0100

    Gelée qui se déforme sur click dessus (gestion avec des ressorts).

---
+++ Jelly/Jelly.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace Jelly
+{
+    public class Jelly
+    {
+        #region Variables privées
+        private int _radius;
+        private float _tension;
+        private float _dampening;
+        #endregion
+
+        #region Propriétés
+        public float Layer { get; set; } = 1f;
+        public Spring[] Springs { get; private set; }
+        public bool Remove { get; set; }
+        public Vector2 Position { get; set; }
+        public int DeltaDegrees { get; set; }
+        public int Radius
+        {
+            get => _radius;
+            set
+            {
+                _radius = value;
+                for (int i = 0; i < Springs.Length; i++)
+                {
+                    ref Spring s = ref Springs[i];
+                    s.TargetValue = value;
+                    if (s.Value == 0)
+                        s.Value = s.TargetValue;
+                }
+            }
+        }
+        public float Spread { get; set; } = 0.25f;
+        public float Tension
+        {
+            get => _tension;
+            set
+            {
+                _tension = value;
+                for (int i = 0; i < Springs.Length; i++)
+                {
+                    ref Spring s = ref Springs[i];
+                    s.Tension = value;
+                }
+            }
+        }
+        public float Dampening
+        {
+            get => _dampening;
+            set
+            {
+                _dampening = value;
+                for (int i = 0; i < Springs.Length; i++)
+                {
+                    ref Spring s = ref Springs[i];
+                    s.Dampening = value;
+                }
+            }
+        }
+        #endregion
+
+        #region Constructeur
+        public Jelly(Vector2 pPosition, int pRadius, int pDeltaDegrees = 1)
+        {
+            DeltaDegrees = pDeltaDegrees;
+            int numberOfSprings = (int)360 / pDeltaDegrees;
+            Springs = new Spring[numberOfSprings];
+            Position = pPosition;
+            Radius = pRadius;
+            Tension = 0.001f;
+            Dampening = 0.015f;
+        }
+        #endregion
+
+        #region Splash
+        public void Splash(Vector2 pPosition)
+        {
+            Vector2 d = pPosition - Position;
+            float dist = (float)Math.Sqrt(Math.Pow(d.X, 2) + Math.Pow(d.Y, 2));
+            if (dist < Radius)
+            {
+                float force = dist - Radius;
+                float angle = MathHelper.ToDegrees((float)Math.Atan2(d.Y, d.X));
+                int index = ((int)Math.Round((angle / DeltaDegrees)) + Springs.Length) % Springs.Length;
+                Springs[index].Velocity = force;
+            }
+        }
+        #endregion
+
+        #region Update
+        public void Update(GameTime gameTime)
+        {
+            #region Update des ressorts
+            for (int i = 0; i < Springs.Length; i++)
+            {
+                ref Spring s = ref Springs[i];
+                s.Update();
+            }
+            #endregion
+
+            #region Propagation des ressorts voisins
+            float[] leftDeltas = new float[Springs.Length];
+            float[] rightDeltas = new float[Springs.Length];
+            for (int i = 0; i < Springs.Length; i++)
+            {
+                ref Spring s0 = ref Springs[(i - 1 + Springs.Length) % Springs.Length];
+                ref Spring s1 = ref Springs[i];
+                ref Spring s2 = ref Springs[(i + 1) % Springs.Length];
+                leftDeltas[i] = Spread * (s1.Value - s0.Value);
+                s0.Velocity += leftDeltas[i];
+                rightDeltas[i] = Spread * (s1.Value - s2.Value);
+                s2.Velocity += rightDeltas[i];
+            }
+
+            for (int i = 1; i < Springs.Length - 1; i++)
+            {
+                ref Spring s0 = ref Springs[i - 1];
+                ref Spring s2 = ref Springs[i + 1];
+                s0.Value += leftDeltas[i];
+                s2.Value += rightDeltas[i];
+            }
+            #endregion
+        }
+        #endregion
+
+        #region Draw
+        public void Draw(PrimitiveBatch primitiveBatch, GameTime gameTime)
+        {
+            Color midnightBlue = new Color(0, 15, 40) * 0.9f;
+            Color lightBlue = new Color(0.2f, 0.5f, 1f) * 0.8f;
+
+            for (int i = 0; i < Springs.Length; i++)
+            {
+                Spring s1 = Springs[i];
+                Spring s2 = Springs[(i + 1) % Springs.Length];
+
+                float angleS1 = MathHelper.ToRadians(i * DeltaDegrees);
+                float angleS2 = MathHelper.ToRadians((i + 1) * DeltaDegrees);
+
+                Vector2 p1 = new Vector2(s1.Value * (float)Math.Cos(angleS1), s1.Value * (float)Math.Sin(angleS1));
+                Vector2 p2 = new Vector2(s2.Value * (float)Math.Cos(angleS2), s2.Value * (float)Math.Sin(angleS2));
+
+                primitiveBatch.AddVertex(Position + p1, midnightBlue);
+                primitiveBatch.AddVertex(Position + p2, midnightBlue);
+                primitiveBatch.AddVertex(Position, lightBlue);
+            }
+        }
+        #endregion
+    }
+}
---
+++ Jelly/MainGame.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Jelly
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class MainGame : Game
+    {
+        private PrimitiveBatch _primitiveBatch;
+        private Jelly _jelly;
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        MouseState oldMouse;
+
+        public MainGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            Viewport screen = graphics.GraphicsDevice.Viewport;
+            _jelly = new Jelly(new Vector2(screen.Width / 2, screen.Height / 2), 100);
+            oldMouse = Mouse.GetState();
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            _primitiveBatch = new PrimitiveBatch(GraphicsDevice);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            _jelly.Update(gameTime);
+
+            #region Sur click effectue un splash
+            MouseState mouse = Mouse.GetState();
+            
+            if (mouse.LeftButton == ButtonState.Pressed && oldMouse.LeftButton == ButtonState.Released)
+            {
+                Vector2 mousePos = mouse.Position.ToVector2();
+                _jelly.Splash(mousePos);
+            }
+            
+            oldMouse = mouse;
+            #endregion
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            _primitiveBatch.Begin(PrimitiveType.TriangleList);
+            _jelly.Draw(_primitiveBatch, gameTime);
+            _primitiveBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
